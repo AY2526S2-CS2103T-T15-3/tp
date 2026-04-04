6e7c2111ff9621b6ddc5ad7dfb0f05ce931c4fd3
Eunwoo <olivianoony62@gmail.com>
2026-04-04 16:56:30 +0800

    Remove unused import in AddCommand

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -12,7 +12,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import java.util.List;
 
 import seedu.address.commons.util.ToStringBuilder;
-import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
